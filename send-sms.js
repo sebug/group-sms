b794bf739ab36491df58e76d17c9926eb79830c2
sebug <sebastian.gfeller@gmail.com>
2019-04-15 17:39:51 +0200

    Now using eCall

--- send-sms.js
+++ send-sms.js
@@ -1,83 +1,32 @@
 const https = require('https');
 
-// Get Access token method
-// Doesn't seems to work, passing HTTP basic credentials directly
-const getAccessToken = (callback) => {
-    console.log('Getting access token');
-    const clientKey = process.env.GROUP_SMS_CLIENT_KEY;
-    const clientSecret = process.env.GROUP_SMS_CLIENT_SECRET;
-    if (!clientKey) {
-	throw new Error("Please specify environment variable GROUP_SMS_CLIENT_KEY");
-    }
-    if (!clientSecret) {
-	throw new Error("Please specify environment variable GROUP_SMS_CLIENT_SECRET");
-    }
-
-    const data = 'grant_type=client_credentials';
-
-    const options = {
-	host: 'consent.swisscom.com',
-	port: 443,
-	path: '/o/oauth2/token',
-	method: 'POST',
-	headers: {
-	    Authorization: 'Basic ' + new Buffer(clientKey + ':' + clientSecret).toString('base64'),
-	    'Content-Length': data.length,
-	    'Content-Type': 'application/x-www-form-urlencoded'
-	}
-    };
-
-    const req = https.request(options, (res) => {
-	console.log(`status code: ${res.statusCode}`);
-
-	res.on('data', (d) => {
-	    process.stdout.write(d);
-	});
-    });
-
-    req.on('error', (error) => {
-	console.error(error);
-    });
-
-    req.write(data);
-    req.end();
-};
+const eCallDomain = "www1.ecall.ch";
+const sendFormat = "/ecallurl/ecallurl.ASP?WCI=Interface&Function=SendPage&Address={address}&Message={message}&AccountName={accountName}&AccountPassword={accountPassword}"
 
 const sendSMS = (targetNumber, message) => {
-    const from = process.env.SMS_SENDER;
-    const clientID = process.env.GROUP_SMS_CLIENT_KEY;
-    const clientSecret = process.env.GROUP_SMS_CLIENT_SECRET;
-    if (!from) {
-	throw new Error("Please specify environment variable SMS_SENDER");
+    const eCallAccountName = process.env.ECALL_ACCOUNT_NAME;
+    const eCallAccountPassword = process.env.ECALL_ACCOUNT_PASSWORD;
+
+    if (!eCallAccountName) {
+	throw new Error("Please specify ECALL_ACCOUNT_NAME");
     }
-    if (!clientID) {
-	throw new Error("Please specify environment variable GROUP_SMS_CLIENT_KEY");
+    if (!eCallAccountPassword) {
+	throw new Error("Please specify ECALL_ACCOUNT_PASSWORD");
     }
-    if (!clientSecret) {
-	throw new Error("Please specify environment variable GROUP_SMS_CLIENT_SECRET");
-    }
-    const data = JSON.stringify({
-	from: from,
-	to: targetNumber,
-	text: message
-    });
-    const requestID = 'gsms-' + Math.random();
-    console.log("SCS-Request-ID: " + requestID);
-    const headers = {
-	'Content-Type': 'application/json',
-	'Content-Length': data.length,
-	'SCS-Version': 2,
-	'client_id': clientID,
-	'SCS-Request-ID': requestID,
-	'Accept': 'application/json'
-    };
+    
+    const requestPath = sendFormat.replace('{accountName}',encodeURIComponent(eCallAccountName))
+	  .replace('{accountPassword}', encodeURIComponent(eCallAccountPassword))
+	  .replace('{address}', encodeURIComponent(targetNumber))
+	  .replace('{message}', encodeURIComponent(message));
+
     const options = {
-	hostname: 'api.swisscom.com',
+	hostname: eCallDomain,
 	port: 443,
-	path: '/messaging/sms',
-	method: 'POST',
-	headers: headers
+	path: requestPath,
+	method: 'GET'
     };
+
+    console.log(requestPath);
 
     const req = https.request(options, (res) => {
 	console.log(`status code: ${res.statusCode}`);
@@ -89,11 +38,6 @@ const sendSMS = (targetNumber, message) => {
 	});
     });
 
-    req.on('error', (error) => {
-	console.error(error);
-    });
-
-    req.write(data);
     req.end();
 };
 
