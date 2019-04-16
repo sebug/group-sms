da486de2401144b8fe29323ed6fc31ad5de5784a
sebug <sebastian.gfeller@gmail.com>
2019-04-16 11:53:58 +0200

    No need for getting authentication token

--- send-sms-swisscom.js
+++ send-sms-swisscom.js
@@ -1,48 +1,5 @@
 const https = require('https');
 
-// Get Access token method
-// Doesn't seems to work, passing HTTP basic credentials directly
-const getAccessToken = (callback) => {
-    console.log('Getting access token');
-    const clientKey = process.env.GROUP_SMS_CLIENT_KEY;
-    const clientSecret = process.env.GROUP_SMS_CLIENT_SECRET;
-    if (!clientKey) {
-	throw new Error("Please specify environment variable GROUP_SMS_CLIENT_KEY");
-    }
-    if (!clientSecret) {
-	throw new Error("Please specify environment variable GROUP_SMS_CLIENT_SECRET");
-    }
-
-    const data = 'grant_type=client_credentials';
-
-    const options = {
-	host: 'consent.swisscom.com',
-	port: 443,
-	path: '/o/oauth2/token',
-	method: 'POST',
-	headers: {
-	    Authorization: 'Basic ' + new Buffer(clientKey + ':' + clientSecret).toString('base64'),
-	    'Content-Length': data.length,
-	    'Content-Type': 'application/x-www-form-urlencoded'
-	}
-    };
-
-    const req = https.request(options, (res) => {
-	console.log(`status code: ${res.statusCode}`);
-
-	res.on('data', (d) => {
-	    process.stdout.write(d);
-	});
-    });
-
-    req.on('error', (error) => {
-	console.error(error);
-    });
-
-    req.write(data);
-    req.end();
-};
-
 const sendSMS = (targetNumber, message) => {
     const from = process.env.SMS_SENDER;
     const clientID = process.env.GROUP_SMS_CLIENT_KEY;
