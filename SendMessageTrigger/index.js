d04d0dbfd11e96e2d343447dac46b1289d688ec1
sebug <sebastian.gfeller@gmail.com>
2019-04-16 10:13:54 +0200

    Actually escaping in function, not just command line

--- SendMessageTrigger/index.js
+++ SendMessageTrigger/index.js
@@ -69,11 +69,17 @@ const returnLink = '<a href="/api/SendMessageTrigger">Nouveau message</a>';
 const eCallDomain = "www1.ecall.ch";
 const sendFormat = "/ecallurl/ecallurl.ASP?WCI=Interface&Function=SendPage&Address={address}&Message={message}&AccountName={accountName}&AccountPassword={accountPassword}";
 
+// The current API doesn't really receive the params as
+// UTF-8, but instead as specified in https://www.ecall.ch/fileadmin/user_upload/ecall/ecall_entwickler/DS-Beschreibung_HTTP_HTTPS-Zugang.pdf
+// Replace accordingly
+const replaceUTF8Escapes = (text) =>
+      text.replace('%C3%A8','%E8');
+
 const sendSMS = (context, eCallAccountName, eCallAccountPassword, targetNumber, message, success, error) => {
     const requestPath = sendFormat.replace('{accountName}',encodeURIComponent(eCallAccountName))
 	  .replace('{accountPassword}', encodeURIComponent(eCallAccountPassword))
 	  .replace('{address}', encodeURIComponent(targetNumber))
-	  .replace('{message}', encodeURIComponent(message));
+	  .replace('{message}', replaceUTF8Escapes(encodeURIComponent(message)));
 
     const options = {
 	hostname: eCallDomain,
